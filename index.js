b365bc76d1fe34f952f89f8f8b9ab864cf6884e6
MartinCravero <82352756+MartinCravero@users.noreply.github.com>
2021-05-31 09:16:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+
+let boton = document.getElementById('boton')
+let buscador = document.getElementById('buscador')
+let listaAnime = []
+let infoAnime2 = document.getElementById('cuerpo')
+
+
+async function getAnime (anime){
+    let resuts = await fetch(`https://api.jikan.moe/v3/search/anime?q=${anime}`)
+        results = await reults.json()
+        return results
+    }
+
+
+    function renderView (infoAnime) {
+        for (let i = 0; i < infoAnime.results.length; i++) {
+            infoAnime2.innerHTML = `<h1>${infoAnime.results[i].title}</h1> <img src="${infoAnime}.results[i].image_url}" alt="poster">`
+            infoAnime2.appendChild = newItem 
+        }
+    }
+    
+    function buscarAnime() {
+        let anime = buscador.value
+        boton.addEventListener ('click',()=>{
+            getAnime(anime)
+                .then ((response) => renderView(response))
+                .catch ((error) => console.error(error))
+            buscador.value = ''    
+        })
+    }
+}
